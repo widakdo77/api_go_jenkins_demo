5af746413e0af3dcbad324d8679d81143cbe9ccc
widakdo77 <64023850+widakdo77@users.noreply.github.com>
2024-08-21 14:18:29 +0700

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -22,7 +22,7 @@ func TestHandler(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := "Hello, World v1b"
+	expected := "Hello, salam sehat"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
